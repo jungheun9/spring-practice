e5edafd9090625b14cf3ef5edf2eb116f0f6da04
Cat Hanbit <jungheun99@naver.com>
2021-04-20 22:51:47 +0900

    Amend controller test
    because we add auth function, we have to certify whether user or not.
    add that logic to test code

--- src/main/java/com/kukucorn/study/springboot/Application.java
+++ src/main/java/com/kukucorn/study/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+// @EnableJpaAuditing // JPA Auditing 활성화  @WebMvcTest 를 사용하게 될 때 최소한의 entity가 없다보니 에러 발생 이를 분리해준다.
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/kukucorn/study/springboot/HelloControllerTest.java
+++ src/test/java/com/kukucorn/study/springboot/HelloControllerTest.java
@@ -1,22 +1,27 @@
 package com.kukucorn.study.springboot;
 
+import com.kukucorn.study.springboot.config.auth.SecurityConfig;
 import com.kukucorn.study.springboot.web.HelloController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.is;
 
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles="USER")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
@@ -26,6 +31,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
