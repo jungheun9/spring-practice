5c897361e5a19672dc6dd6ba8c5ed2a80329ed40
Cat Hanbit <jungheun99@naver.com>
2021-04-20 20:56:39 +0900

    Study OAuth2
    use google login api,
    add auth service, dto, config
    add user domain
    amend index view, controller

--- src/main/java/com/kukucorn/study/springboot/web/IndexController.java
+++ src/main/java/com/kukucorn/study/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kukucorn.study.springboot.web;
 
+import com.kukucorn.study.springboot.config.auth.dto.SessionUser;
 import com.kukucorn.study.springboot.service.posts.PostsService;
 import com.kukucorn.study.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsSevice;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsSevice.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index"; // Controller 에서 문자열을 반환하면 mustache starter 가 자동으로 경로와 확장자를 지정해준다.
     }
 
